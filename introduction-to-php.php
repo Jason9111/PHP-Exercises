70e0d3415dada2d7d1336707d23d358bb1027804
Jason9111 <46696287+Jason9111@users.noreply.github.com>
2019-01-15 22:15:56 -0500

    Add files via upload

---
+++ introduction-to-php.php
@@ -0,0 +1,104 @@
+Online Class: Introduction to PHP
+/*
++---+
+| 1 | 
++---+
+Print the sentence: Welcome to PHP!
+*/
+ -->  
+<?php 
+
+echo "Welcome to PHP!";
+
+echo "<hr style=\"margin 1rem 0\">";
+
+?>
+
+
+
+<!-- 
+/* 
++---+
+| 2 | 
++---+
+Print the sentence: PHP stands for "Hypertext Preprocessor"!
+*/
+ -->
+<?php 
+	echo 'PHP stands for "Hypertext Preprpcessor"!';
+	
+	echo "<hr style=\"margin 1rem 0\">";
+
+?>
+
+
+
+<!-- /* 
++---+
+| 3 | 
++---+
+Print the following sentence as a paragraph: PHP stands for "Hypertext Preprocessor"!
+*/ -->
+
+<?php 
+
+	echo '<p>PHP stands for "Hypertext Preprpcessor"!</p>';
+	echo "<hr style=\"margin 1rem 0\">";
+
+
+?>
+
+
+
+
+<!-- /* 
++---+
+| 4 | 
++---+
+Print the following acronyms - every acronym in a new line:
+
+HTML - Stands for Hypertext Markup Language
+CSS - Stands for Cascading Stylesheet
+JS - Stands for JavaScript
+PHP - Stands for Hypertext Preprocessor
+SQL - Stands for Structural Query Language
+*/ -->
+   
+<?php 
+echo 'HTML - Stands for Hypertext Markup Language <br /> CSS - Stands for Cascading Stylesheet <br /> JS - Stands for JavaScript <br /> PHP - Stands for Hypertext Preprocessor <br />SQL - Stands for Structural Query Language';
+
+echo "<hr style=\"margin 1rem 0\">";
+?>   
+   
+   
+
+<!-- /* 
++---+
+| 5 | 
++---+
+Print the following acronyms in unordered list - <ul> ... </ul>:
+
+HTML - Stands for Hypertext Markup Language
+CSS - Stands for Cascading Stylesheet
+JS - Stands for JavaScript
+PHP - Stands for Hypertext Preprocessor
+SQL - Stands for Structural Query Language
+*/ -->
+
+
+<?php 
+	echo '  
+		<ul>
+			<li>HTML - Stands for Hypertext Markup Language</li>
+			<li>CSS - Stands for Cascading Stylesheet</li>
+			<li>JS - Stands for JavaScript</li>
+			<li>PHP - Stands for Hypertext Preprocessor</li>
+			<li>SQL - Stands for Structural Query Language</li>
+		</ul
+	'
+?>
+
+
+
+
+
